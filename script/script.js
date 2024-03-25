7f4f1d46cd3264c2d58db7cd9090986082d96eda
Nicolas <haubrichtnicolas@gmail.com>
2024-03-25 20:53:42 -0300

    Commit teste

--- script/script.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    var resposta = confirm("Se você tem mais de 18 anos e deseja continuar clique em OK");
-    
-    if (!resposta) {
-        window.location.href = "./pages/erro.html";
-    }
-}
