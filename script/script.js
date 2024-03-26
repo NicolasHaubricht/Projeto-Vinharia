f264e11943df2b322baeb74aad421bc8f2b7a9dc
Nicolas <haubrichtnicolas@gmail.com>
2024-03-26 12:04:32 -0300

    Tipos de Vinho criado e editado usando JS

---
+++ script/script.js
@@ -0,0 +1,30 @@
+const carrossel = document.querySelector('.carrossel');
+const containerCarrossel = document.querySelector('.container-carrossel');
+const imagens = document.querySelectorAll('.container-carrossel img');
+const btnAnterior = document.querySelector('.anterior');
+const btnProximo = document.querySelector('.proximo');
+
+
+let indiceImagemAtual = 0;
+
+imagens.forEach((imagem, indice) => {
+    if (indice !== indiceImagemAtual) {
+        imagem.style.display = 'none';
+    }
+});
+
+function mostrarProximaImagem() {
+    imagens[indiceImagemAtual].style.display = 'none';
+    indiceImagemAtual = (indiceImagemAtual + 1) % imagens.length;
+    imagens[indiceImagemAtual].style.display = 'block';
+}
+
+function mostrarImagemAnterior() {
+    imagens[indiceImagemAtual].style.display = 'none';
+    indiceImagemAtual = (indiceImagemAtual - 1 + imagens.length) % imagens.length;
+    imagens[indiceImagemAtual].style.display = 'block';
+}
+
+btnProximo.addEventListener('click', mostrarProximaImagem);
+
+btnAnterior.addEventListener('click', mostrarImagemAnterior);
